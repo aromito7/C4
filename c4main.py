430022ff4ced0103030ea6c2ec7b0bff45770cab
Alexander Romito <aromito7@gmail.com>
2022-04-25 17:14:04 -0400

    Added an exit button and fixed the main menu.

--- c4main.py
+++ c4main.py
@@ -288,24 +288,31 @@ class Game:
 		background.setFill('White')
 		background.draw(win)
 		
-		left_button = Rectangle(Point(0, height//2), Point(width//buttons, height))
+		left_button = Rectangle(Point(0, 2*height//3), Point(width//buttons, height))
 		left_button.setFill('White')
 		left_button.draw(win)
-		left_button_text = Text(Point(width//4, (height*3)//4), 'Play Basic AI')
+		left_button_text = Text(Point(width//6, (height*5)//6), 'Play Basic AI')
 		left_button_text.setTextColor("Black")
-		left_button_text.setSize(16)
+		left_button_text.setSize(12)
 		left_button_text.draw(win)
 
-		right_button = Rectangle(Point(width//buttons, height//2), Point(width, height))
+		right_button = Rectangle(Point(width//buttons, 2*height//3), Point(2*width//buttons, height))
 		right_button.setFill('White')
 		right_button.draw(win)
-		right_button_text = Text(Point((width*3)//4, (height*3)//4), 'Neural Network')
+		right_button_text = Text(Point((width)//2, (height*5)//6), 'Neural Network')
 		right_button_text.setTextColor("Black")
-		right_button_text.setSize(16)
+		right_button_text.setSize(12)
 		right_button_text.draw(win)
 
+		exit_button = Rectangle(Point(2*width//buttons, 2*height//3), Point(width, height))		
+		exit_button.setFill('White')
+		exit_button.draw(win)
+		exit_button_text = Text(Point(5*width//6, 5*height//6), 'Exit')
+		exit_button_text.setTextColor("Black")
+		exit_button_text.setSize(12)
+		exit_button_text.draw(win)
 
-		label = Text(Point(width//2, height//4), 'Connect Four')
+		label = Text(Point(width//2, height//3), 'Connect Four')
 		label.setTextColor("Red")
 		label.setSize(36)
 		label.draw(win)
@@ -313,14 +320,14 @@ class Game:
 		while stay_on_menu:
 			click = win.getMouse()
 			x, y = click.x, click.y
-			if y > height//2 and x < width//2:
+			if y > height//2 and x < width//3:
 				p1 = Player()
 				p2 = Player("PL")
 				game = Game(p1,p2)
 				game.start()
 
 				stay_on_menu = True
-			if y > height//2 and x > width//2:
+			if y > height//2 and x > 2 * width//3:
 				stay_on_menu = False
 				win.close()
 
